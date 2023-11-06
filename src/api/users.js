6a6983a06fe128668014c6f4eebc743283f33784
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-11-06 17:30:45 +0200

    fix fetch

--- src/api/users.js
+++ src/api/users.js
@@ -1,6 +1,6 @@
 const API_BASE = 'https://randomuser.me/api/';
 
 export const getUsers = ({results = 10, page = 1}) => {
-    return fetch (`${API_BASE}?results=${results}&page=${page}&seed=fd2023`)
+    return fetch (`${API_BASE}?results=${results}&page=${page}`)
     .then (response => response.json())
 }
