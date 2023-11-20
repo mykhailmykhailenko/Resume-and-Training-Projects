bb9b754e369a1498f5854be4d413111eaa4a602f
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-11-20 13:52:15 +0200

    adding links on landing pages

--- src/App.js
+++ src/App.js
@@ -25,12 +25,14 @@ class App extends React.Component {
             <BrowserRouter>
                 <h1 className="header">My training projects</h1>
                 <ul>
-                    <li><a className="links" href="https://github.com/mykhailmykhailenko?tab=repositories" target="blank">My GIT account</a></li>
-                    <li><Link className="links" to="/">Go to ResumePage</Link></li>
+                    <li><a className="links" href="https://github.com/mykhailmykhailenko?tab=repositories" target="blank">Go to my GIT account</a></li>
+                    <li><Link className="links" to="/">Go to Resume page</Link></li>
+                    <li><a className="links" href="https://avilio-landingpage.netlify.app/" target="blank">Go to Avilio landing page (adaptive layout of landing page)</a></li>
+                    <li><a className="links" href="https://traveller-landingpage.netlify.app/" target="blank">Go to Traveller landing page (adaptive layout of landing page)</a></li>
                     <li><Link className="links" to="/userDashboard">Go to UserDashboard (fetch, pagination, sort and filter of users)</Link></li>
                     <li><Link className="links" to="/messengerPage">Go to MessengerPage (useState, useEffect, useReducer, useRef, useContext, fetch )</Link></li>
                     <li><Link className="links" to="/CurrencyConverter">Go to CurrencyConverter(useState, useEffect, useReducer, fetch)</Link></li>
-                    <li><Link className="links" to="/loginPage">Go to LoginPage (with validation and errors on npm formik)</Link></li>
+                    <li><Link className="links" to="/loginPage">Go to Login and Registration page (with validation and errors on npm formik)</Link></li>
                     <li><Link className="links" to="/singUpFormWithYup">Go to SignUpForm (with validation on npm yup)</Link></li>
                     <li><Link className="links" to="/productLoader">Go to ProductLoader (custom hook useData for getting list of products with fetch) </Link></li>
                     <li><Link className="links" to="/timer">Go to Timer (training hooks: useState, useEffect. npm date-fns, setTimeout)</Link></li>
